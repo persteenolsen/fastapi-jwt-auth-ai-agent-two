2aeb4049f2580750c5c057b43cec5dd8af2aa440
Per Olsen <persteen22@gmail.com>
2026-06-08 18:30:24 +0200

    Adjusted the app initialization in main.py

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ from routes import router
 # APP INIT
 # -----------------------------
 app = FastAPI(
-    title="FastAPI + JWT Authentication + Tool-Calling AI Agent",
-    description="08-06-2026 - FastAPI using JWT Authentication hosted at Render serving AI tool calling agent",
+    title="FastAPI with JWT Auth serving a Tool-Calling AI Agent",
+    description="08-06-2026 - FastAPI with JWT Auth serving a Tool-Calling AI Agent, built to use tools like Calculator and Wikipedia if decided by the Agent",
     version="1.0.0",
     contact={
         "name": "Per Olsen",
         "url": "https://persteenolsen.netlify.app",
     },
 )
-
 
 app.include_router(router)
