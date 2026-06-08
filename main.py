3467c3999c08db1046071d1909f613cdbbf334b8
Per Olsen <persteen22@gmail.com>
2026-06-08 14:44:52 +0200

    Updated main.py with header info

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from routes import router
 # APP INIT
 # -----------------------------
 app = FastAPI(
-    title="FastAPI + JWT Auth + AI Tool Calling Agent",
+    title="FastAPI + JWT Auth + Tool-Calling AI Agent",
     description="08-06-2026 - FastAPI using JWT Auth hosted at Render serving AI tool calling agent",
     version="1.0.0",
     contact={
